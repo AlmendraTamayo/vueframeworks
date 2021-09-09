359b6d72a80c2e5297040a4de2a58e0914b9c3e4
AlmendraTamayo <almendra.tamayo@gmail.com>
2021-09-08 21:57:37 -0300

    se sube desafio

---
+++ components/CharacterCard.vue
@@ -0,0 +1,42 @@
+<template>
+  <!--   <v-container>
+    <v-row>
+      <v-col
+        v-for="personaje in personajes"
+        :key="personaje.id"
+        cols="12"
+        md="6"
+        lg="4"
+      > -->
+  <v-hover v-slot="{ hover }">
+    <v-card
+      v-ripple
+      class="mx-auto card"
+      max-width="344"
+      :elevation="hover ? 12 : 2"
+      @click="$emit('click', personaje)"
+    >
+      <v-img :src="personaje.imagen" height="200px"></v-img>
+
+      <v-card-title> {{ personaje.nombre }}</v-card-title>
+    </v-card>
+  </v-hover>
+  <!--       </v-col>
+    </v-row>
+  </v-container> -->
+</template>
+
+<script>
+export default {
+  props: {
+    personaje: { type: Object, required: true },
+  },
+}
+</script>
+
+<style scoped>
+.card {
+  transition: all 0.2s linear;
+  cursor: pointer;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col
+        cols="12"
+        sm="6"
+        lg="4"
+        v-for="personaje in $store.state.personajes"
+        :key="personaje.id"
+      >
+        <CharacterCard
+          :personaje="personaje"
+          @click="$router.push({ path: `/detalle/${$event.id}` })"
+        />
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+<script>
+import CharacterCard from '../components/CharacterCard.vue'
+export default {
+  components: { CharacterCard },
+}
+</script>
